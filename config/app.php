31a1ce04137f53b135ddf04041362bf49e771bc0
Fandy Akhmad <fanjavaid@gmail.com>
2016-04-01 06:40:35 +0000

    Configure autoload for Form/HTML and PDF Libraries

--- config/app.php
+++ config/app.php
@@ -156,6 +156,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+	Collective\Html\HtmlServiceProvider::class,
+	Barryvdh\DomPDF\ServiceProvider::class,
+
     ],
 
     /*
@@ -201,7 +204,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+	
+	'PDF' => Barryvdh\DomPDF\Facade::class,
+	'Form' => Collective\Html\FormFacade::class,
+      	'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
